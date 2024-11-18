e115e16de6a3bf7f81716ae18e5cc6f91ae231df
MAY 06 <MAY 06@LAB2-34>
2024-11-18 14:33:07 +0700

    Add project files.

---
+++ bai15_ARRAY1/bai15_ARRAY1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{// Create an array of strings
+	string car[5] = { "volvo","BMW","FORD", "MAZA", "TESLA" };
+	//Loop through strings
+	for (int i = 0; i < 5; i++) {
+		cout << car [i] << "\n";
+	}
+
+}
